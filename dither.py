c7125ea8f7f0c56ad030a771d1808403df144c74
Mika Rantanen <mika.rantanen@iki.fi>
2022-07-26 23:43:49 +0300

    Add command line support

---
+++ dither.py
@@ -0,0 +1,67 @@
+import sys
+import numpy as np
+from PIL import Image
+
+
+dithering_matrix = tuple((x / 64.0) - 0.5 for x in (
+     0, 32,  8, 40,  2, 34, 10, 42,
+    48, 16, 56, 24, 50, 18, 58, 26,
+    12, 44,  4, 36, 14, 46,  6, 38,
+    60, 28, 52, 20, 62, 30, 54, 22,
+     3, 35, 11, 43,  1, 33,  9, 41,
+    51, 19, 59, 27, 49, 17, 57, 25,
+    15, 47,  7, 39, 13, 45,  5, 37,
+    63, 31, 55, 23, 61, 29, 53, 21
+))
+
+
+def get_dithering_threshold(pos):
+    """Returns a dithering threshold for the given position."""
+
+    x = int(pos[0]) % 8
+    y = int(pos[1]) % 8
+    return dithering_matrix[x + y * 8]
+
+
+def get_lut_color(lut, color):
+    """Returns a value from the given lookup table for the given color."""
+
+    size = lut.height
+    rgb = np.floor(np.divide(color, 256.0) * size)
+    x = rgb[0] + rgb[2] * size + 0.5 / lut.width
+    y = rgb[1] + 0.5 / lut.height
+    return lut.getpixel((x, y))
+
+
+def dither_image(image, lut):
+    """Dithers the given image using the given lookup table."""
+
+    output = Image.new("RGB", image.size)
+
+    for pos in np.ndindex((image.width, image.height)):
+        color = image.getpixel(pos)
+        color_lightness = get_lut_color(lut, color)[3]
+
+        spread = np.clip(color_lightness * 2, 50, 150)
+        threshold = get_dithering_threshold(pos)
+        dithering_color = np.clip(np.add(color, spread * threshold), 0, 255)
+
+        new_color = get_lut_color(lut, dithering_color)
+        output.putpixel(pos, new_color)
+
+    return output
+
+
+def main(argv):
+    if len(argv) != 3:
+        print("usage: dither.py image_filename lut_filename output_filename")
+        sys.exit(2)
+
+    image = Image.open(argv[0]).convert("RGB")
+    lut = Image.open(argv[1])
+    output = dither_image(image, lut)
+    output.save(argv[2])
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
--- generate_lut.py
+++ generate_lut.py
@@ -1,5 +1,8 @@
+import sys
 import numpy as np
 from PIL import Image
+
+
 class Palette:
     def __init__(self, rgb_color_values):
         """Initializes a palette using the given list of RGB color values [r, g, b, ...]."""
@@ -8,6 +11,7 @@ class Palette:
         self.num_colors = len(rgb_array)
         self.rgb = tuple(tuple(x) for x in rgb_array)
         self.lab = tuple(rgb_to_lab(x) for x in rgb_array)
+
 
     def get_nearest_color(self, rgb_color):
         """Returns a palette color that is most similar to the given color.""" 
@@ -24,6 +28,7 @@ class Palette:
 
         return self.rgb[nearest_color_index]
 
+
 def rgb_to_lab(rgb_color):
     """Converts the given RGB color to the CIELAB color space."""
 
@@ -37,11 +42,13 @@ def rgb_to_lab(rgb_color):
 
     return (116 * xyz[1] - 16, 500 * (xyz[0] - xyz[1]), 200 * (xyz[1] - xyz[2]))
 
+
 def get_lab_distance(lab_color_0, lab_color_1):
     """Calculates a distance between two colors in the CIELAB color space using the CIE76 color difference formula."""
 
     v = np.subtract(lab_color_1, lab_color_0)
     return np.linalg.norm(v)
+
 
 def get_palette(filename):
     """Returns a palette generated from the given image file."""
@@ -52,6 +59,7 @@ def get_palette(filename):
         raise Exception("The image does not have a palette") 
 
     return Palette(color_values)
+
 
 def generate_lut(palette, size):
     """Returns a lookup table image generated from the given palette."""
@@ -68,51 +76,15 @@ def generate_lut(palette, size):
     return image
 
 
-palette = get_palette("palette-c64.png")
-lut = generate_lut(palette, 64)
-lut.save("color-grading.png")
+def main(argv):
+    if len(argv) != 3:
+        print("usage: generate_lut.py palette_filename output_filename lut_size")
+        sys.exit(2)
+
+    palette = get_palette(argv[0])
+    lut = generate_lut(palette, int(argv[2]))
+    lut.save(argv[1])
 
 
-dithering_matrix = tuple((x / 64.0) - 0.5 for x in (
-     0, 32,  8, 40,  2, 34, 10, 42,
-    48, 16, 56, 24, 50, 18, 58, 26,
-    12, 44,  4, 36, 14, 46,  6, 38,
-    60, 28, 52, 20, 62, 30, 54, 22,
-     3, 35, 11, 43,  1, 33,  9, 41,
-    51, 19, 59, 27, 49, 17, 57, 25,
-    15, 47,  7, 39, 13, 45,  5, 37,
-    63, 31, 55, 23, 61, 29, 53, 21
-))
-
-def get_dithering_threshold(pos):
-    """Returns a dithering threshold for the given position."""
-
-    x = int(pos[0]) % 8
-    y = int(pos[1]) % 8
-    return dithering_matrix[x + y * 8]
-
-def get_lut_color(lut, color):
-    """Returns a value from the given lookup table for the given color."""
-
-    size = lut.height
-    rgb = np.floor(np.divide(color, 256.0) * size)
-    x = rgb[0] + rgb[2] * size + 0.5 / lut.width
-    y = rgb[1] + 0.5 / lut.height
-    return lut.getpixel((x, y))
-
-
-lut = Image.open("color-grading.png")
-test_image = Image.open("test.png")
-
-for pos in np.ndindex((test_image.width, test_image.height)):
-    color = test_image.getpixel(pos)
-    color_lightness = get_lut_color(lut, color)[3]
-
-    spread = np.clip(color_lightness * 2, 50, 150)
-    threshold = get_dithering_threshold(pos)
-    dithering_color = np.clip(np.add(color, spread * threshold), 0, 255)
-
-    new_color = get_lut_color(lut, dithering_color)
-    test_image.putpixel(pos, new_color)
-
-test_image.save("test-palette.png")
+if __name__ == "__main__":
+   main(sys.argv[1:])
